d150fc8b3cea8322d02788a9b93c69ee06453d51
gyoenge <gyoengist@gm.gist.ac.kr>
2024-05-11 03:24:39 +0900

    feat: implement FastAPI endpoint for image upload (backend)

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import os
+import shutil
 
 # FastAPI 인스턴스를 만듭니다.
 app = FastAPI()
 
-# 루트 엔드포인트를 정의합니다.
-@app.get("/")
+
+@app.get("/")  # 루트 엔드포인트를 정의합니다.
 async def read_root():
     return {"message": "Hello, World!"}
 
-# 기타 엔드포인트를 추가할 수 있습니다.
-@app.get("/items/{item_id}")
+
+@app.get("/items/{item_id}")  # 기타 엔드포인트를 추가할 수 있습니다.
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+    """
+        이미지 파일 전송 받아서 로컬에 저장 
+    """
+    folder = 'uploads'
+    os.makedirs(folder, exist_ok=True)
+    file_location = f"{folder}/{file.filename}"
+
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"filename": file.filename}
