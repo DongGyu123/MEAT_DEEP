bb85602653f37c36b3aab4874ff90c58a0ad7b75
임동규 <DongGyu123>
2024-05-13 21:13:40 +0900

    백엔드 거의 완성 진짜

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
+import json
+from fastapi import FastAPI, File, UploadFile, Response
 import os
 import shutil
 from fastapi.responses import FileResponse
@@ -23,22 +24,32 @@ async def create_upload_file(file: UploadFile = File(...)):
     """
         이미지 파일 전송 받아서 로컬에 저장 
     """
+    image_path = "C:/Users/daniel/Desktop/MEAT/backend/gogi/runs/detect/predict/a.jpg"
     dir_path = os.path.dirname(image_path)
-
-
-    folder_to_delete = os.path.join(dir_path, "predict")
-    if os.path.exists(folder_to_delete):
-        shutil.rmtree(folder_to_delete)
-        print(f"Folder {folder_to_delete} deleted successfully.")
-    else:
-        print(f"Folder {folder_to_delete} does not exist.")
+    
     folder = 'uploads'
     os.makedirs(folder, exist_ok=True)
-    file_location = f"{folder}/{file.filename}"
+    file_location = f"{folder}/file.jpg"
 
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    result=model.predict(source='./uploads/gogi.jpg', save=True)
-    image_path = "C:/Users/daniel/Desktop/MEAT/backend/gogi/runs/detect/predict/a.jpg"
+    result=model.predict(source='./uploads/file.jpg', save=True)
+    data=result[0].boxes
+    respond = {
+    'cls': data.cls.tolist(),  # 리스트로 변환하여 추가
+    'conf': data.conf.tolist(),
+    'id': data.id,
+    'xyxyn': data.xyxyn.tolist()  # 리스트로 변환하여 추가
+    }
+    print('the value of result: ', type(respond['cls']), type(respond['conf']), type(respond['id']), type(respond['xyxyn']))
+   
+    json_string = json.dumps(respond)
+    dir_path = os.path.dirname(__file__)
+    # folder_to_delete = os.path.join(dir_path, "predict")
+    # if os.path.exists(folder_to_delete):
+    #     shutil.rmtree(folder_to_delete)
+    #     print(f"Folder {folder_to_delete} deleted successfully.")
+    # else:
+    #     print(f"Folder {folder_to_delete} does not exist.")
         
-    return FileResponse(image_path)
+    return json_string
